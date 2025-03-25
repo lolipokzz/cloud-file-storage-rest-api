dbcf89c533f3526b8821610346b595827119674c
bobby <lolipok12@rambler.ru>
2025-03-25 10:31:25 +0300

    refactor: add private access modifiers in dto

--- src/main/java/org/example/cloudfilestoragerestapi/dto/response/MessageResponseDto.java
+++ src/main/java/org/example/cloudfilestoragerestapi/dto/response/MessageResponseDto.java
@@ -9,5 +9,5 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MessageResponseDto {
-    String message;
+    private String message;
 }
--- src/main/java/org/example/cloudfilestoragerestapi/dto/response/UserResponseDto.java
+++ src/main/java/org/example/cloudfilestoragerestapi/dto/response/UserResponseDto.java
@@ -10,5 +10,5 @@ import lombok.Setter;
 @Builder
 public class UserResponseDto {
 
-    String username;
+    private String username;
 }
